248f59c912ef6fbe0845ea3a81f5a0b36cfe9f8c
“callmegemy” <callmegemy@gmail.com>
2025-01-09 18:59:11 +0200

    Category & Product models with migrations

---
+++ eec_task/database/migrations/2025_01_09_164545_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('code')->unique();
+            $table->string('image')->nullable();
+            $table->decimal('price')->default(0);
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
